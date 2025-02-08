b9316146511e41a17c897913775d87e38fdf0c46
alistairjevans <alistair@betterstack.com>
2025-02-08 14:26:08 +0000

    Sort of something

--- ext/hyper_ruby/src/lib.rs
+++ ext/hyper_ruby/src/lib.rs
@@ -1,43 +1,23 @@
-use hyper_util::rt::TokioIo;
-use magnus::{function, method, prelude::*, Error as MagnusError, Ruby, Value};
+use lucchetto::{without_gvl, GvlSafe};
+use magnus::{block, function, method, prelude::*, Error as MagnusError, RClass, RObject, Ruby, Value};
+use bytes::Bytes;
 
-use http_body_util::Full;
-use hyper::body::Bytes;
-//#[cfg(feature = "server")]
-use hyper::server::conn::http1;
-use hyper::service::service_fn;
-use hyper::{Request, Response};
+use warp::Filter;
 use std::cell::RefCell;
-use std::convert::Infallible;
-use std::error::Error;
 use std::net::SocketAddr;
-use tokio::net::{TcpListener, UnixListener};
+
+use tokio::net::UnixListener;
+use tokio_stream::wrappers::UnixListenerStream;
 
 use std::sync::Arc;
 use tokio::sync::Mutex;
 use tokio::task::JoinHandle;
-// This would normally come from the `hyper-util` crate, but we can't depend
-// on that here because it would be a cyclical dependency.
-use tokio;
 
+use parking_lot;
 
-#[derive(Clone)]
-// An Executor that uses the tokio runtime.
-pub struct TokioExecutor;
-
-// Implement the `hyper::rt::Executor` trait for `TokioExecutor` so that it can be used to spawn
-// tasks in the hyper runtime.
-// An Executor allows us to manage execution of tasks which can help us improve the efficiency and
-// scalability of the server.
-impl<F> hyper::rt::Executor<F> for TokioExecutor
-where
-    F: std::future::Future + Send + 'static,
-    F::Output: Send + 'static,
-{
-    fn execute(&self, fut: F) {
-        tokio::task::spawn(fut);
-    }
-}
+struct ThreadSafeValue(magnus::Value);
+unsafe impl Send for ThreadSafeValue {}
+unsafe impl Sync for ThreadSafeValue {}
 
 #[derive(Clone)]
 struct ServerConfig {
@@ -49,34 +29,26 @@ impl ServerConfig {
     fn new() -> Self {
         Self {
             bind_address: String::from("127.0.0.1:3000"),
-            worker_threads: 4,
+            worker_threads: 4
         }
     }
 }
-
-enum Listener {
-    Unix(UnixListener),
-    Tcp(TcpListener),
-}
-
-// Add this trait
-trait AsyncStream: tokio::io::AsyncRead + tokio::io::AsyncWrite + Send + Unpin {}
-impl<T: tokio::io::AsyncRead + tokio::io::AsyncWrite + Send + Unpin> AsyncStream for T {}
-
-// Update the type alias
-type BoxedStream = Box<dyn AsyncStream>;
 
 #[magnus::wrap(class = "HyperRuby::Server")]
 struct Server {
     server_handle: Arc<Mutex<Option<JoinHandle<()>>>>,
     config: RefCell<ServerConfig>,
+    handler: Option<ThreadSafeValue>
 }
+
+impl GvlSafe for Server {}
 
 impl Server {
     pub fn new() -> Self {
         Self {
             server_handle: Arc::new(Mutex::new(None)),
             config: RefCell::new(ServerConfig::new()),
+            handler: None
         }
     }
 
@@ -91,42 +63,10 @@ impl Server {
         Ok(())
     }
 
-    async fn handler(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
-        let mut response = Response::new(Full::new(Bytes::from("Hello, World!")));
-        response.headers_mut().insert(
-            hyper::header::SERVER,
-            hyper::header::HeaderValue::from_static("hyper-ruby"),
-        );
-        Ok(response)
-    }
-
-    async fn accept_connection(listener: &Listener) -> Option<BoxedStream> {
-        match listener {
-            Listener::Unix(l) => match l.accept().await {
-                Ok((stream, _)) => {
-                    Some(Box::new(stream))
-                },
-                Err(e) => {
-                    eprintln!("Failed to accept Unix connection: {}", e);
-                    None
-                }
-            },
-            Listener::Tcp(l) => match l.accept().await {
-                Ok((stream, _)) => {
-                    Some(Box::new(stream))
-                },
-                Err(e) => {
-                    eprintln!("Failed to accept TCP connection: {}", e);
-                    None
-                }
-            },
-        }
-    }
-
-    pub fn start(&self) -> Result<(), MagnusError> {
-        let server_handle = self.server_handle.clone();
-            
-        let config = self.config.borrow().clone();
+    #[without_gvl]
+    pub fn start(slf: Server) -> Result<(), MagnusError> {
+        let config = slf.config.borrow().clone();
+ 
 
         let rt = tokio::runtime::Builder::new_multi_thread()
             .worker_threads(config.worker_threads)
@@ -136,72 +76,79 @@ impl Server {
 
         println!("Starting server");
         
-        // Enter the runtime context and block on the initialization
         rt.block_on(async {
-            println!("Binding to: {}", config.bind_address);
-            let addr = &config.bind_address;
-            let listener = if addr.starts_with('/') {
-                match UnixListener::bind(addr) {
-                    Ok(l) => Listener::Unix(l),
-                    Err(e) => {
-                        eprintln!("Failed to bind Unix socket: {}", e);
-                        return Result::<(), MagnusError>::Ok(());
-                    }
-                }
-            } else {
-                match addr.parse::<SocketAddr>() {
-                    Ok(sock_addr) => match TcpListener::bind(sock_addr).await {
-                        Ok(l) => Listener::Tcp(l),
-                        Err(e) => {
-                            eprintln!("Failed to bind TCP socket: {}", e);
-                            return Ok(());
-                        }
-                    },
-                    Err(e) => {
-                        eprintln!("Invalid socket address: {}", e);
-                        return Ok(());
-                    }
-                }
-            };
-
-            println!("Listening on: {}", addr);
-
             let server_task = tokio::spawn(async move {
-                loop {
-                    let stream = match Self::accept_connection(&listener).await {
-                        Some(stream) => stream,
-                        None => continue,
-                    };
-
-                    let io = TokioIo::new(stream);
-                    let handler = Self::handler;
-
-                    tokio::spawn(async move {
-                        let service = service_fn(handler);
-                        match http1::Builder::new()
-                            .serve_connection(io, service)
-                            .await
-                        {
-                            Ok(_) => {},
-                            Err(err) => {
-                                eprintln!("Error serving HTTP/1.1 connection: {:?}", err);
-                                if let Some(source) = err.source() {
-                                    eprintln!("Caused by: {:?}", source);
+                let handler = handler.clone();
+                let any_route = warp::any()
+                    .and(warp::filters::method::method())
+                    .and(warp::filters::path::full())
+                    .and(warp::header::headers_cloned())
+                    .and(warp::body::bytes())
+                    .map(move |method: warp::http::Method, 
+                            path: warp::path::FullPath,
+                            headers: warp::http::HeaderMap,
+                            body: bytes::Bytes| {
+                        let handler = handler.read();
+                        // // Create a hash to store request info
+                        // let req_info = magnus::RHash::new();
+                        // req_info.aset(magnus::Symbol::new("method"), method.as_str()).unwrap();
+                        // req_info.aset(magnus::Symbol::new("path"), path.as_str()).unwrap();
+                        
+                        // // Convert headers to a hash
+                        // let header_hash = magnus::RHash::new();
+                        // for (key, value) in headers.iter() {
+                        //     if let Ok(v) = value.to_str() {
+                        //         header_hash.aset(key.as_str(), v).unwrap();
+                        //     }
+                        // }
+                        // req_info.aset(magnus::Symbol::new("headers"), header_hash).unwrap();
+                        
+                        // // Convert body to string and add to hash
+                        // req_info.aset(magnus::Symbol::new("body"), String::from_utf8_lossy(&body).into_owned()).unwrap();
+                        
+                        let result = match handler.0.funcall("call", ("req_info",)) {
+                            Ok(response) => {
+                                if let Ok(response_str) = String::try_convert(response) {
+                                    response_str
+                                } else {
+                                    "Error: Handler response could not be converted to string".to_string()
                                 }
                             }
-                        }
+                            Err(_) => "Error: Handler call failed".to_string()
+                        };
+
+                        warp::reply::with_status(
+                            result,
+                            warp::http::StatusCode::OK
+                        )
                     });
+
+                if config.bind_address.starts_with("unix:") {
+                    let path = config.bind_address.trim_start_matches("unix:");
+                
+                    let listener = UnixListener::bind(path).unwrap();
+                    let incoming = UnixListenerStream::new(listener);
+                    warp::serve(any_route)
+                        .run_incoming(incoming)
+                        .await
+                } else {
+                    let addr: SocketAddr = config.bind_address.parse()
+                        .expect("invalid address format");
+                    warp::serve(any_route)
+                        .run(addr)
+                        .await;
                 }
             });
 
-            let mut handle = server_handle.lock().await;
+            let mut handle = self.server_handle.lock().await;
             *handle = Some(server_task);
             
-            Ok(())
+            Ok::<(), MagnusError>(())
         })?;
 
         // Keep the runtime alive
         std::thread::spawn(move || {
+            magnus::embed::init_from_rust();  // Still need this for the background thread
             rt.block_on(async {
                 loop {
                     tokio::time::sleep(std::time::Duration::from_secs(1)).await;
@@ -235,7 +182,7 @@ fn init(ruby: &Ruby) -> Result<(), MagnusError> {
     let server_class = module.define_class("Server", ruby.class_object())?;
     server_class.define_singleton_method("new", function!(Server::new, 0))?;
     server_class.define_method("configure", method!(Server::configure, 1))?;
-    server_class.define_method("start", method!(Server::start, 0))?;
+    server_class.define_method("start", method!(Server::start, 1))?;
     server_class.define_method("stop", method!(Server::stop, 0))?;
 
     Ok(())
--- test/test_hyper_ruby.rb
+++ test/test_hyper_ruby.rb
@@ -10,8 +10,15 @@ class TestHyperRuby < Minitest::Test
   def test_it_does_something_useful
     server = HyperRuby::Server.new
     server.configure({ bind_address: "127.0.0.1:3010" })
-    server.start
+    handler = Handler.new
+    server.start(handler)
     gets
     server.stop
+  end
+
+  class Handler
+    def call(req_info)
+      "Hello, world!"
+    end
   end
 end
